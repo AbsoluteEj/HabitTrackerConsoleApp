0c9e22a0399e089f4ff1b2d265ad27f217972135
AbsoluteEj <ejsadiarin@gmail.com>
2022-08-04 03:36:05 +0800

    added similar logic

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-Habit Tracker PSEUDOCODE 
+Habit Tracker PSEUDOCODE
 CRUD app
 
 UI choices:
+
 - 0 for close
 - 1 for view all records
 - 2 for creating habit record
 - 3 for updating record
 - 4 for deleting record
 
+Like in the math game:
+
+- use methods/func 
+- switch cases
+- use message arguments on methods
--- UI-habit-tracker/Program.cs
+++ UI-habit-tracker/Program.cs
@@ -3,9 +3,56 @@ Console.WriteLine("Welcome to the Habit Tracker Console Application\n");
 
 Console.WriteLine("MAIN MENU\n");
 
-Console.WriteLine("What would you like to do?");
-Console.WriteLine("Type 0 - To Close Application");
-Console.WriteLine("Type 1 - To View All Records");
-Console.WriteLine("Type 2 - To Create/Insert Habit Record");
-Console.WriteLine("Type 3 - To Update Habit Record");
-Console.WriteLine("Type 4 - To Delete Habit Record");
+Console.WriteLine("What would you like to do? Please choose from the options below:");
+var select = Console.ReadLine();
+
+if (select == null)
+{
+    Console.WriteLine("Please type a valid input");
+    Console.ReadLine();
+}
+switch (int.Parse(select.Trim()))
+{
+    case "0":
+        CloseApp("Thank you for your time!");
+    case "1":
+        ViewRecord("Enter 1 - To View All Records");
+    case "2":
+        CreateRecord("Enter 2 - To Create/Insert Habit Record");
+    case "3":
+        UpdateRecord("Enter 3 - To Update Habit Record");
+    case "4":
+        DeleteRecord("Enter 4 - To Delete Habit Record");
+    default:
+        Console.WriteLine("Your input should be valid");
+}
+
+
+
+
+void CloseApp(string message)
+{
+    Console.WriteLine(message);
+    Console.ReadLine();
+    Environment.Exit(1);
+}
+void ViewRecord(string message)
+{
+    Console.WriteLine(message);
+    Console.ReadLine();
+}
+void CreateRecord(string message)
+{
+    Console.WriteLine(message);
+    Console.ReadLine();
+}
+void UpdateRecord(string message)
+{
+    Console.WriteLine(message);
+    Console.ReadLine();
+}
+void DeleteRecord(string message)
+{
+    Console.WriteLine(message);
+    Console.ReadLine();
+}
